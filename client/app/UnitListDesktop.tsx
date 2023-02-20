1951b5183e46309f3210c474c0d840aa3a498411
William <s3864077@rmit.edu.vn>
2023-02-20 15:53:05 +0700

    keep thead on top

--- client/app/UnitListDesktop.tsx
+++ client/app/UnitListDesktop.tsx
@@ -5,7 +5,7 @@ import RateColor from "./RateColor";
 function UnitListDesktop({ data }: any) {
   return (
     <table className="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <thead className="text-xs sticky top-0 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
           <th className="px-6 py-3">Character</th>
           <th className="px-6 py-3">Name</th>
